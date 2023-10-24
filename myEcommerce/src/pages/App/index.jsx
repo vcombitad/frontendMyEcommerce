1590f2f361e5bd38ff8f0f354cd9dba86f6d099f
vcombitad <vcombitad@ucentral.edu.co>
2023-10-24 11:08:33 -0500

    Se configuran las rutas de cada componente del PAGE, usando react-router-dom

--- myEcommerce/src/pages/App/index.jsx
+++ myEcommerce/src/pages/App/index.jsx
@@ -1,3 +1,4 @@
+import {useRoutes, BrowserRouter} from 'react-router-dom'
 import Home from '../Home'
 import MyShop from '../myShop'
 import NotFound from '../notFound'
@@ -5,19 +6,22 @@ import Shop from '../shop'
 import SignIn from '../signIn'
 import './App.css'
 
-function App() {
-  
-  return (
-    <>
-      <div className='bg-orange-300'>
-        <Home/>
-        <MyShop/>
-        <NotFound/>
-        <Shop/>
-        <SignIn/>
-      </div>
-    </>
-  )
+const AppRoutes =()=>{
+  let routes=useRoutes([
+    { path: '/', element: <Home/>},
+    { path: '/my-shop', element: <MyShop/>},
+    { path: '/shop', element: <Shop/>},
+    { path: '/sign-in', element: <SignIn/>},
+    { path: '/*', element: <NotFound/>},
+  ])
+  return routes
 }
 
+const App=()=>{
+  return (
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+  )
+}
 export default App
